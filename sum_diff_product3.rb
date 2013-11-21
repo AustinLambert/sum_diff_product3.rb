37328de6cb83a7efbb825b51b90e7f4783c35403
murkano <calicea@chicagotechacademy.org>
2013-11-21 15:40:59 -0600

    HAHA

---
+++ sum_diff_product3.rb
@@ -0,0 +1,36 @@
+class Numbers
+
+	def initialize(first_number , second_number)
+		@first_number = first_number
+
+		@second_number = second_number
+end
+
+ attr_accessor 'first_number', 'second_number'
+
+        def sum
+                @first_number + @second_number
+        end
+
+        def diff
+                @first_number - @second_number
+        end
+
+        def product
+                @first_number * @second_number
+        end
+
+end
+
+puts "enter two numbers"
+first_number , second_number = gets.chomp.to_i.split
+
+result = Numbers.new first_number, second_number
+
+puts ""
+
+puts result.sum
+
+puts result.diff
+
+puts result.product
